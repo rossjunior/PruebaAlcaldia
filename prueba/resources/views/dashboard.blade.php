4fd23770a9ff81f8783ea612e1c0640fae443d94
Ross Jr <rossjunior9436@hotmail.com>
2024-05-23 00:19:19 -0500

    Se realiza ajuste para usar correctamente el datatable y documentación de instalación en REAME.md donde tambien se incluyen consultas SQL

--- prueba/resources/views/dashboard.blade.php
+++ prueba/resources/views/dashboard.blade.php
@@ -9,7 +9,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <!-- Agrega un contenedor para la tabla -->
                     <div class="table-responsive">
                         <table id="users-table" class="table table-striped">
                             <thead>
@@ -30,6 +29,14 @@
                                     </tr>
                                 @endforeach
                             </tbody>
+                            <tfood>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Correo</th>
+                                    <th>Municipio</th>
+                                    <th>Creado</th>
+                                </tr>
+                            </tfood>
                         </table>
                     </div>
                 </div>
@@ -37,32 +44,4 @@
         </div>
     </div>
 
-    @push('scripts')
-    <script>
-        $(document).ready(function () {
-            // Inicializa el DataTable con opciones personalizadas
-            $('#users-table').DataTable({
-                searching: true, // Habilita la funcionalidad de búsqueda
-                paging: true, // Habilita la paginación
-                ordering: true, // Habilita la ordenación
-                lengthChange: false, // Oculta el control para cambiar el número de filas por página
-                info: true, // Muestra la información de paginación
-                responsive: true, // Habilita la funcionalidad de tabla receptiva
-                language: {
-                    search: "_INPUT_", // Personaliza el texto del cuadro de búsqueda
-                    searchPlaceholder: "Buscar...", // Personaliza el marcador de posición del cuadro de búsqueda
-                    paginate: {
-                        first: "Primero", // Personaliza el botón "Primero" de paginación
-                        previous: "Anterior", // Personaliza el botón "Anterior" de paginación
-                        next: "Siguiente", // Personaliza el botón "Siguiente" de paginación
-                        last: "Último" // Personaliza el botón "Último" de paginación
-                    },
-                    info: "Mostrando _START_ a _END_ de _TOTAL_ registros", // Personaliza el texto de información de paginación
-                    infoEmpty: "Mostrando 0 a 0 de 0 registros", // Personaliza el texto de información cuando no hay registros
-                    infoFiltered: "(filtrado de _MAX_ registros totales)" // Personaliza el texto de información cuando se aplica filtrado
-                }
-            });
-        });
-    </script>
-    @endpush
 </x-app-layout>
--- prueba/resources/views/layouts/app.blade.php
+++ prueba/resources/views/layouts/app.blade.php
@@ -10,17 +10,19 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">-->
 
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-                <!-- Scripts -->
-                <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-        <!-- DataTables CSS -->
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
-
+       
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        
+        <link href="https://cdn.datatables.net/2.0.7/css/dataTables.bootstrap5.css" rel="stylesheet">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <!-- DataTables JS -->
-        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/2.0.7/js/dataTables.bootstrap5.js"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -41,4 +43,30 @@
             </main>
         </div>
     </body>
+
+    <script>
+        $(document).ready(function () {
+            $('#users-table').DataTable({
+                searching: true, // Habilita la funcionalidad de búsqueda
+                paging: true, // Habilita la paginación
+                ordering: true, // Habilita la ordenación
+                lengthChange: false, // Oculta el control para cambiar el número de filas por página
+                info: true, // Muestra la información de paginación
+                responsive: true, // Habilita la funcionalidad de tabla receptiva
+                language: {
+                    search: "_INPUT_", // Personaliza el texto del cuadro de búsqueda
+                    searchPlaceholder: "Buscar...", // Personaliza el marcador de posición del cuadro de búsqueda
+                    paginate: {
+                        first: "Primero", // Personaliza el botón "Primero" de paginación
+                        previous: "Anterior", // Personaliza el botón "Anterior" de paginación
+                        next: "Siguiente", // Personaliza el botón "Siguiente" de paginación
+                        last: "Último" // Personaliza el botón "Último" de paginación
+                    },
+                    info: "Mostrando _START_ a _END_ de _TOTAL_ registros", // Personaliza el texto de información de paginación
+                    infoEmpty: "Mostrando 0 a 0 de 0 registros", // Personaliza el texto de información cuando no hay registros
+                    infoFiltered: "(filtrado de _MAX_ registros totales)" // Personaliza el texto de información cuando se aplica filtrado
+                }
+            });
+        });
+    </script>
 </html>
